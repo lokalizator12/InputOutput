e5f18bcdfa74aede2fd130df6b7bf2f57b0fd1b5
lokalizator12 <83495533+lokalizator12@users.noreply.github.com>
2022-05-07 18:33:58 +0200

    Input Num line Output value line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,35 @@
+import jdk.jshell.spi.ExecutionControlProvider;
+
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
         File directory = new File("Folder1");
-
         File fileWithName = new File(directory, "Names.txt");
 
-        try (Reader reader = new InputStreamReader(new FileInputStream(fileWithName))) {
-            char[] array = new char[16];
-            int count = reader.read(array);
-            StringBuilder stringBuilder = new StringBuilder();
-            while (count > 0) {
-                stringBuilder.append(new String(array, 0, count));
-                count = reader.read(array);
+
+        Scanner scanner = new Scanner(System.in);
+        String line = "";
+
+        while (!line.equals("stop")) {
+            System.out.println("Please enter number line: ");
+            line = scanner.nextLine();
+
+            try (RandomAccessFile randomAccessFile = new RandomAccessFile(fileWithName, "rw");
+            ) {
+                randomAccessFile.seek(Integer.parseInt(line));
+                String rline = randomAccessFile.readLine();
+                System.out.println(rline);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            String[] names = stringBuilder.toString().split(" ");
-            Arrays.stream(names)
-                    .filter((name -> name.startsWith("A")))
-                    .forEach(System.out::println);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+
         }
     }
+
+
 }
